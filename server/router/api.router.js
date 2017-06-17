8a1ab4973e469567ea3d1fce4f3b500914aa3c2b
Dustin <dustin@baldlion.com>
2017-06-17 18:54:19 -0400

    removed dead files

--- server/router/api.router.js
+++ server/router/api.router.js
@@ -5,8 +5,5 @@ import Posts from '../controllers/posts.controller'
 const router = express.Router()
 
 router.get('/posts', Posts.index)
-router.get('/posts/:postId', Posts.getById)
-router.post('/posts/:postId', Posts.update)
-router.post('/posts', Posts.add)
 
 export default router
